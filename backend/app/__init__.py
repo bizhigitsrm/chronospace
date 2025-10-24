5805a51e9542b636be9dd8f02647b95d56be520c
Syrym <ehaaatyou@gmail.com>
2025-10-25 00:22:37 +0500

    fixing

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -4,4 +4,4 @@ This file makes `backend/app` importable as a package which stabilizes
 relative imports during tests, CI and when running via uvicorn.
 """
 
-__all__ = ["app_main", "api", "db", "core", "schemas"]
+# __all__ = ["schemas"]
